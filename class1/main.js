8f33d255e43c6a559bf533c9979a3c5a011dcbeb
Bilal Raza <success9262@gmail.com>
2023-10-23 14:22:15 +0500

    make a repo

---
+++ class1/main.js
@@ -0,0 +1,51 @@
+// DATA TYPES
+//PRIMITIVE
+// String
+// Boolean
+// Number
+// Bigint
+// Null and Undefined
+// Symbol
+//NON-PRIMITIVE
+//Array
+//Function
+//Object
+// TypeScript Special Types
+//any
+// Never
+// Void
+// Unknown Type
+//enum
+// number
+var number1 = 12;
+// // STRING
+var names1 = "bilal";
+var myroll1 = "12";
+// //BOOLEAN
+var isMan1 = true;
+// // undefined
+var myNamed1 = "muhammad";
+myNamed1.toUpperCase();
+myNamed1 = "bilal";
+console.log(typeof (myNamed1));
+//any
+var id = "rfgh";
+id = 12;
+//NON-PRIMITIVE DATA TYPES
+//ARRAY
+var hobbies = ["cricket", "gym", "dqwg"];
+console.log(hobbies);
+//object
+var person = {
+    names: "arfat",
+    fatherName: "lsj",
+    id: {
+        name: ""
+    }
+};
+console.log(person.names);
+function isGender() {
+    console.log("Man");
+}
+isGender();
+isGender();
---
+++ class1/main.ts
@@ -0,0 +1,75 @@
+
+// DATA TYPES
+
+//PRIMITIVE
+
+// String
+// Boolean
+// Number
+// Bigint
+// Null and Undefined
+// Symbol
+
+//NON-PRIMITIVE
+
+//Array
+//Function
+//Object
+
+// TypeScript Special Types
+
+//any
+// Never
+// Void
+// Unknown Type
+//enum
+
+
+
+// number
+let number1: number = 12
+
+// // STRING
+let names1: string = "bilal"
+let myroll1: string = "12"
+
+// //BOOLEAN
+let isMan1: boolean = true
+
+// // undefined
+let myNamed1: string = "muhammad";
+myNamed1.toUpperCase()
+myNamed1 = "bilal"
+console.log(typeof (myNamed1));
+
+
+//any
+let id: any = "rfgh"
+id = 12
+
+//NON-PRIMITIVE DATA TYPES
+
+//ARRAY
+let hobbies: string[] = ["cricket", "gym", "dqwg"]
+console.log(hobbies);
+
+//object
+let person = {
+    names: "arfat",
+    fatherName: "lsj",
+    id: {
+        name: ""
+    }
+
+}
+
+console.log(person.names);
+
+function isGender() {
+    
+}
+ isGender()
+ isGender()
+
+
+
