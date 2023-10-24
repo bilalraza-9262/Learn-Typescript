e8293b76039939c5ca61c63b2aa75203b62dc270
Bilal Raza <success9262@gmail.com>
2023-10-24 23:37:46 +0500

    define string methods

--- class4/main.js
+++ class4/main.js
@@ -1,7 +1,8 @@
-var message = "my name is ";
-var myName = "Bilal";
-console.log("".concat(message, " ").concat(myName));
-var ternory = myName ? "true" : message ? "true" : false;
+"use strict";
+let message = "my name is ";
+let myName = "Bilal";
+console.log(`${message} ${myName}`);
+let ternory = myName ? "true" : message ? "true" : false;
 console.log(ternory);
 if (myName) {
     if (message) {
@@ -18,7 +19,7 @@ else {
 ;
 console.log("my na mes \rbilal");
 // STRING METHOD IN TS
-var my_message = "     Hello World   ";
+let my_message = "Hello World    ";
 //strings are immutatable
 //for length
 console.log(my_message.length);
@@ -28,7 +29,18 @@ console.log(my_message.charAt(0));
 console.log(my_message.startsWith("H"));
 console.log(my_message.charCodeAt(0));
 console.log(my_message.split(""));
-var my_copy = my_message.slice(-3);
+let my_copy = my_message.slice(-5);
 console.log(my_copy);
 console.log(my_message.length);
 console.log(my_message.trim().length);
+console.log(my_message.includes("h"));
+console.log(my_message.indexOf("H"));
+console.log(my_message.lastIndexOf("o"));
+console.log(my_message.replace("Hello", "Hy"));
+let new_message = " Bilal";
+console.log(my_message.concat(new_message));
+console.log(my_message.repeat(2));
+let my_number = 20;
+let num2 = 30;
+my_number.toString();
+console.log(num2 + my_number);
--- class4/main.ts
+++ class4/main.ts
@@ -13,19 +13,16 @@ if (myName) {
         console.log("true");
     } else {
         console.log(false);
-
     };
-
 } else {
     console.log("false");
-
 };
 
 console.log("my na mes \rbilal");
 
 // STRING METHOD IN TS
 
-let my_message:string ="     Hello World   ";
+let my_message:string ="Hello World    ";
 
 //strings are immutatable
 //for length
@@ -43,9 +40,26 @@ console.log(my_message.charCodeAt(0));
 
 console.log(my_message.split(""));
 
- let my_copy:string = my_message.slice(-3)
+ let my_copy:string = my_message.slice(-5)
+
  console.log(my_copy);
  console.log(my_message.length);
  
  console.log(my_message.trim().length);
+
+ console.log(my_message.includes("h"));
+ console.log(my_message.indexOf("H"));
+ 
+ console.log(my_message.lastIndexOf("o"));
+ 
+ console.log(my_message.replace("Hello","Hy"));
+
+ let new_message:string = " Bilal";
+ console.log(my_message.concat(new_message));
+ console.log(my_message.repeat(2));
+ 
+ let my_number=20
+ let num2=30
+ my_number.toString()
+ console.log(num2 + my_number);
  
